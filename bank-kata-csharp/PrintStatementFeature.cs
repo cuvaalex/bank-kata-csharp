3ace53e11886436523a52a3ce9bd3927b9cefc11
Alex Cuva <alexandre.cuva@gmail.com>
2017-06-25 08:28:32 +0200

    first commit

---
+++ bank-kata-csharp/PrintStatementFeature.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace bankkatacsharp
+{
+    [TestFixture()]
+    public class PrintStatementFeature
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
